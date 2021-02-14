951ea0531ab8512bf4d13dd4d35c8bae1d5642ab
Simon Tye <simontye@uark.edu>
2021-02-14 12:08:35 -0600

    yeeted

--- R/maddog.R
+++ R/maddog.R
@@ -1,40 +1,40 @@
 #######################################################################################
 
-divlist        <- c("set1", "set2", "set3", "set4")
-names(divlist) <- divlist
-seqlist        <- c("rainbow")
-names(seqlist) <- seqlist
+set.list        <- c("north", "south", "east", "west")
+names(set.list) <- set.list
+seq.list        <- c("rainbow")
+names(seq.list) <- seq.list
 
-divnum         <- rep(6, length(divlist))
-names(divnum)  <- divlist
-seqnum         <- rep(10, length(seqlist))
-names(seqnum)  <- seqlist
+set.num         <- rep(6, length(set.list))
+names(set.num)  <- set.list
+seq.num         <- rep(10, length(seq.list))
+names(seq.num)  <- seq.list
 
-namelist       <- c(divlist, seqlist)
-maxcolors      <- c(divnum, seqnum)
-catlist        <- rep(c("div", "seq"),
-                      c(length(divlist), length(seqlist)))
+list.name       <- c(set.list, seq.list)
+col.max         <- c(set.num, seq.num)
+list.cat        <- rep(c("div", "seq"),
+                      c(length(set.list), length(seq.list)))
 
-maddog.info    <- data.frame(maxcolors = maxcolors,
-                             category  = catlist,
-                             row.names = namelist)
+maddog.info     <- data.frame(col.max   = col.max,
+                              category  = list.cat,
+                              row.names = list.name)
 
 #######################################################################################
 
 # Function: returns a maddog color palette (e.g., scale_fill_manual(values = maddog(n, "name")))
-maddog <- function(n, name) {
-  if(!(name %in% namelist)) {
+maddog <- function(name, n) {
+  if(!(name %in% list.name)) {
     stop(paste(name, "is not a valid palette name for maddog\n"))}
 
   if(n < 3) {
     warning(paste("Minimal value for n in palette",name,"is 3. Returning palette with 3 different levels\n"))
-    return(maddog(3, name))}
+    return(maddog(name, 3))}
 
-  if(n > maxcolors[which(name == namelist)]) {
-    warning(paste("Maximum value for n in palette",name,"is",maxcolors[which(name == namelist)]),". Returning palette with maximum levels\n")
-    return(maddog(maxcolors[which(name == namelist)], name))}
+  if(n > col.max[which(name == list.name)]) {
+    warning(paste("Maximum value for n in palette",name,"is",col.max[which(name == list.name)]),". Returning palette with maximum levels\n")
+    return(name, maddog(col.max[which(name == list.name)]))}
   switch(name,
-         set1 = switch(n-2,
+         north = switch(n-2,
                        rgb(c(214, 0,   139),
                            c(152, 151, 177),
                            c(41,  158, 78),
@@ -51,7 +51,24 @@ maddog <- function(n, name) {
                            c(152, 86,  100, 151, 177, 96),
                            c(41,  63,  81,  158, 78,  96),
                            maxColorValue = 255)),
-         set2 = switch(n-2,
+         south = switch(n-2,
+                        rgb(c(181, 0,  78),
+                            c(35, 151, 26),
+                            c(37, 158, 26),
+                            maxColorValue = 255),
+                        rgb(c(222, 181, 0,   78),
+                            c(216, 35,  151, 26),
+                            c(142, 37,  158, 26),
+                            maxColorValue = 255),
+                        rgb(c(222, 181, 204, 0,   78),
+                            c(216, 35,  174, 151, 26),
+                            c(142, 37,  106, 158, 26),
+                            maxColorValue = 255),
+                        rgb(c(222, 181, 204, 136, 0,   78),
+                            c(216, 35,  174, 25,  151, 26),
+                            c(142, 37,  106, 27,  158, 26),
+                            maxColorValue = 255)),
+         east = switch(n-2,
                        rgb(c(219, 0,   214),
                            c(53,  151, 152),
                            c(39,  158, 41),
@@ -68,35 +85,22 @@ maddog <- function(n, name) {
                            c(53,  151, 177, 152, 127, 33),
                            c(39,  158, 78,  41,  57,  96),
                            maxColorValue = 255)),
-         set3 = switch(n-2,
-                       rgb(c(214,0,111),
-                           c(113,151,33),
-                           c(47,158,96),maxColorValue=255),
-                       rgb(c(214,0,90,111),
-                           c(113,151,127,33),
-                           c(47,158,57,96),maxColorValue=255),
-                       rgb(c(214,0,90,183,111),
-                           c(113,151,127,86,33),
-                           c(47,158,57,63,96),maxColorValue=255),
-                       rgb(c(214,0,214,90,183,111),
-                           c(113,151,152,127,86,33),
-                           c(47,158,41,57,63,96),maxColorValue=255)),
-         set4 = switch(n-2,
-                       rgb(c(181, 0,  78),
-                           c(35, 151, 26),
-                           c(37, 158, 26),
+         west = switch(n-2,
+                       rgb(c(214, 0,   111),
+                           c(113, 151, 33),
+                           c(47,  158, 96),
                            maxColorValue = 255),
-                       rgb(c(222, 181, 0,   78),
-                           c(216, 35,  151, 26),
-                           c(142, 37,  158, 26),
+                       rgb(c(214, 0,   90, 111),
+                           c(113, 151, 127, 33),
+                           c(47,  158, 57,  96),
                            maxColorValue = 255),
-                       rgb(c(222, 181, 204, 0,   78),
-                           c(216, 35,  174, 151, 26),
-                           c(142, 37,  106, 158, 26),
+                       rgb(c(214, 0,   90,  183, 111),
+                           c(113, 151, 127, 86,  33),
+                           c(47,  158, 57,  63,  96),
                            maxColorValue = 255),
-                       rgb(c(222, 181, 204, 136, 0,   78),
-                           c(216, 35,  174, 25,  151, 26),
-                           c(142, 37,  106, 27,  158, 26),
+                       rgb(c(214, 0,   214, 90,  183, 111),
+                           c(113, 151, 152, 127, 86,  33),
+                           c(47,  158, 41,  57,  63,  96),
                            maxColorValue = 255)),
          rainbow = switch(n-2,
                         rgb(c(0,   214, 181),
@@ -134,69 +138,117 @@ maddog <- function(n, name) {
 
 #######################################################################################
 
+# Make flavors
+redgrapewine    <- rgb(78,  26,  26,  maxColorValue = 255)
+dragonfruit     <- rgb(136, 25,  27,  maxColorValue = 255)
+redsangria      <- rgb(181, 35,  37,  maxColorValue = 255)
+cranberry       <- rgb(219, 53,  39,  maxColorValue = 255)
+tangerinedream  <- rgb(189, 59,  39,  maxColorValue = 255)
+rubygrapefruit  <- rgb(181, 35,  37,  maxColorValue = 255)
+peachesandcream <- rgb(214, 113, 47,  maxColorValue = 255)
+orangejubilee   <- rgb(214, 152, 41,  maxColorValue = 255)
+keylimepie      <- rgb(204, 174, 106, maxColorValue = 255)
+lightningcreek  <- rgb(222, 216, 142, maxColorValue = 255)
+sourapple       <- rgb(139, 177, 78,  maxColorValue = 255)
+spikedmelon     <- rgb(92,  174, 88,  maxColorValue = 255)
+kiwilemon       <- rgb(90,  127, 57,  maxColorValue = 255)
+blueraspberry   <- rgb(0,   151, 158, maxColorValue = 255)
+hawaiianblue    <- rgb(61,  100, 81,  maxColorValue = 255)
+purplerain      <- rgb(111, 33,  96,  maxColorValue = 255)
+
+# Save flavors
+# usethis::use_data(redgrapewine)
+# usethis::use_data(dragonfruit)
+# usethis::use_data(redsangria)
+# usethis::use_data(cranberry)
+# usethis::use_data(tangerinedream)
+# usethis::use_data(rubygrapefruit)
+# usethis::use_data(peachesandcream)
+# usethis::use_data(orangejubilee)
+# usethis::use_data(keylimepie)
+# usethis::use_data(lightningcreek)
+# usethis::use_data(sourapple)
+# usethis::use_data(spikedmelon)
+# usethis::use_data(kiwilemon)
+# usethis::use_data(blueraspberry)
+# usethis::use_data(hawaiianblue)
+# usethis::use_data(purplerain)
+
+#######################################################################################
+
 # Function: displays particular maddog color palette
-display.maddog <- function(n, name) {
-  if(!(name %in% namelist)) {
-    stop(paste(name,"is not a valid palette name for maddog\n"))}
+#maddog.palette <- function(n, name) {
+#  if(!(name %in% list.name)) {
+#    stop(paste(name,"is not a valid palette name for maddog\n"))}
+#
+#  if(n < 3) {
+#    warning(paste("Minimal value for n in palette",name,"is 3. Returning palette with 3 different levels\n"))
+#    return(display.maddog(3,name))}
+#
+#  if(n > col.max[which(name == list.name)]) {
+#    warning(paste("Maximum value for n in palette",name,"is",col.max[which(name == list.name)]),". Returning palette with maximum levels\n")
+#    return(maddog(col.max[which(name == list.name)], name))}
+#
+#  if(length(which(name == set.list))>0) palattr <- "(divergent)"
+#  if(length(which(name == seq.list))>0) palattr <- "(sequential)"
+#  image(x = 1:n, y = 1, z = as.matrix(1:n), col = maddog(n, name),
+#        xlab = paste(name, palattr), ylab = "", xaxt = "n", yaxt = "n", bty = "n")}
 
-  if(n < 3) {
-    warning(paste("Minimal value for n in palette",name,"is 3. Returning palette with 3 different levels\n"))
-    return(display.maddog(3,name))}
-
-  if(n > maxcolors[which(name == namelist)]) {
-    warning(paste("Maximum value for n in palette",name,"is",maxcolors[which(name == namelist)]),". Returning palette with maximum levels\n")
-    return(maddog(maxcolors[which(name == namelist)], name))}
-
-  if(length(which(name == divlist))>0) palattr <- "(divergent)"
-  if(length(which(name == seqlist))>0) palattr <- "(sequential)"
-  image(x = 1:n, y = 1, z = as.matrix(1:n), col = maddog(n, name),
-        xlab = paste(name, palattr), ylab = "", xaxt = "n", yaxt = "n", bty = "n")}
+# Function: displays all maddog flavors
+maddog_flavors <- function(n = NULL, type = "all", select = NULL, exact.n = TRUE) {
+  flav.list = c("redgrapewine", "dragonfruit", "redsangria",
+                "cranberry", "tangerinedream", "rubygrapefruit",
+                "peachesandcrea", "orangejubilee", "keylimepie",
+                "lightningcreek", "sourapple", "spikedmelon",
+                "kiwilemon", "blueraspberry", "hawaiianblue",
+                "purplerain")
+  return(flav.list)}
 
 #######################################################################################
 
 # Function: displays all maddog color palettes
-display.maddog.all <- function (n = NULL, type = "all", select = NULL, exact.n = TRUE) {
-  gaplist          <- ""
-  totallist        <- c(divlist, gaplist, seqlist)
-  names(totallist) <- c(names(divlist), "gap1", names(seqlist))
-  gapnum           <- max(c(divnum, seqnum))
-  totnum           <- c(divnum, gapnum, seqnum)
-  names(totnum)    <- names(totallist)
+maddog_palettes <- function(n = NULL, type = "all", select = NULL, exact.n = TRUE) {
+  gap.list          <- ""
+  total.list        <- c(set.list, gap.list, seq.list)
+  names(total.list) <- c(names(set.list), "gap1", names(seq.list))
+  gap.num           <- max(c(set.num, seq.num))
+  tot.num           <- c(set.num, gap.num, seq.num)
+  names(tot.num)    <- names(total.list)
 
   if (!(type %in% c("div", "seq", "all"))) {
     stop(paste(type, "is not a valid name for a color list\n"))}
-  colorlist <- switch(type, div = divlist, seq = seqlist, all = totallist)
-  maxnum    <- switch(type, div = divnum, seq = seqnum, all = totnum)
+  color.list <- switch(type, div = set.list, seq = seq.list, all = total.list)
+  max.num    <- switch(type, div = set.num,  seq = seq.num,  all = tot.num)
 
-  if(!is.null(select)){colorlist <- colorlist[select]
-  maxnum <- maxnum[select]
+  if(!is.null(select)){color.list <- color.list[select]
+  max.num <- max.num[select]
 
-  if(any(is.na(colorlist)))
+  if(any(is.na(color.list)))
     stop(paste("Invalid value(s) of select: ",
-               paste(select[is.na(colorlist)],
+               paste(select[is.na(color.list)],
                      collapse=" ")))}
 
   palattr <- switch(type, div = "divergent", seq = "sequential", all = "divergent+sequential")
 
-  if(is.null(n))n <- maxnum
+  if(is.null(n))n <- max.num
 
-  if(length(n) == 1)n <- rep(n, length(colorlist))
+  if(length(n) == 1)n <- rep(n, length(color.list))
 
   if(exact.n){
-    keep      <- n <= maxnum
-    colorlist <- colorlist[keep]
+    keep      <- n <= max.num
+    color.list <- color.list[keep]
     n         <- n[keep]
-    maxnum    <- maxnum[keep]}
+    max.num    <- max.num[keep]}
 
-  if (any(n < 3) | exact.n & any(n > maxnum)|
-      length(n) != length(colorlist)){
+  if (any(n < 3) | exact.n & any(n > max.num)|
+      length(n) != length(color.list)){
     warning("Illegal vector of color numbers")
     print(paste(n, collapse=" "))}
 
   n[n < 3]      <- 3
-  n[n > maxnum] <- maxnum[n > maxnum]
+  n[n > max.num] <- max.num[n > max.num]
 
-  nr <- length(colorlist)
+  nr <- length(color.list)
   nc <- max(n)
 
   ylim   <- c(0, nr)
@@ -208,10 +260,11 @@ display.maddog.all <- function (n = NULL, type = "all", select = NULL, exact.n =
 
   for(i in 1:nr)
   {nj <- n[i]
-  if (colorlist[i] == "") next
-  shadi <- maddog(nj, colorlist[i])
-  rect(xleft=0:(nj-1), ybottom = i-1, xright = 1:nj, ytop = i-0.2, col = shadi,
+  if (color.list[i] == "") next
+  shade <- maddog(color.list[i], nj)
+  rect(xleft = 0:(nj - 1), ybottom = i-1, xright = 1:nj, ytop = i - 0.2, col = shade,
        border = "light grey")}
-  text(rep(-0.1, nr), (1:nr)-0.6, labels = colorlist, xpd = TRUE, adj = 1)}
+  text(rep(-0.1, nr), (1:nr) - 0.6, labels = color.list, xpd = TRUE, adj = 1, cex = 0.75)}
+
 #######################################################################################
 
